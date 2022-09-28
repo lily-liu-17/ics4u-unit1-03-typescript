a93ac5152812df8116bba1befeabf6739b25da40
lily-liu-17 <lily.liu@mths.ca>
2022-09-28 01:47:30 +0000

    innit

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const prompt = promptSync()
 const sub = 60.0
 const soup = 105.0
 const pizza = 45.0
+const minute = 60
 
 const food = prompt('Are you heating up soup, sub or pizza : ')
 
@@ -22,13 +23,13 @@ const quantityInput = parseFloat(quantityInputString)
 if (quantityInput <= 3 && quantityInput >= 1) {
   if (food === 'pizza') {
     const cookingTime1 = pizza + 0.5 * pizza * (quantityInput - 1)
-    console.log(`The food will be cooked in ${cookingTime1} seconds`)
+    console.log(`The food will be cooked in ${Math.floor(cookingTime1 / minute)} minute and ${cookingTime1 % minute} seconds`)
   } else if (food === 'sub') {
     const cookingTime2 = sub + 0.5 * sub * (quantityInput - 1)
-    console.log(`The food will be cooked in ${cookingTime2} seconds`)
+    console.log(`The food will be cooked in ${Math.floor(cookingTime2 / minute)} minute and ${cookingTime2 % minute} seconds`)
   } else if (food === 'soup') {
     const cookingTime3 = soup + 0.5 * soup * (quantityInput - 1)
-    console.log(`The food will be cooked in ${cookingTime3} seconds`)
+    console.log(`The food will be cooked in ${Math.floor(cookingTime3 / minute)} minute and ${cookingTime3 % minute} seconds`)
   } else {
     console.log('Invalid Input')
   }
